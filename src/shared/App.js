da17d83935e6c87df0225f766898c6d89d80e79d
김서연 <kim01093314735@gmail.com>
2021-07-25 21:58:54 +0900

    Login Page 추가

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import PostList from '../pages/PostList';
+import Login from '../pages/Login';
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Route path="/" exact component={PostList} />
+        <Route path="/login" exact component={Login} />
       </BrowserRouter>
     </React.Fragment>
   );
