065711d8d4056308476bd1ac495f4e94a19c7761
김서연 <kim01093314735@gmail.com>
2021-07-30 17:33:32 +0900

    Firebase 설정

---
+++ src/shared/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi2iBzNiGlRk2et-9SMPunlFpHC3721fA",
+  authDomain: "image-community-512aa.firebaseapp.com",
+  projectId: "image-community-512aa",
+  storageBucket: "image-community-512aa.appspot.com",
+  messagingSenderId: "580111465685",
+  appId: "1:580111465685:web:242a62cffdcf4c5ebd765c",
+  measurementId: "G-B3RYB6TN1H"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebse.auth();
+
+export {auth};
