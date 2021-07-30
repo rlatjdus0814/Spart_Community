cec0ab23ddb361d780fbaf9dc9680f7dc0847291
김서연 <kim01093314735@gmail.com>
2021-07-30 15:21:25 +0900

    User Modules 추가

---
+++ src/redux/modules/user.js
@@ -0,0 +1,42 @@
+import { createAction, handleActions } from 'redux-actions';
+import { produce } from 'immer';
+import { getCookie, setCookie, deleteCookie } from '../shared/Cookie';
+
+// actions type
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+const GET_USER = 'GET_USER';
+
+// action creators
+const logIn = createAction(LOG_IN, (user) => ({user}));
+const logOut = createAction(LOG_OUT, (user) => ({user}));
+const getUser = createAction(GET_USER, (user) => ({user}));
+
+// initialState 
+const initialState = {
+  user: null,
+  is_login: false,
+};
+
+// reducer
+export default handleActions({
+  [LOG_IN]: (state, action) => produce(state, (draft) => {
+    setCookie('is_login', 'succes');
+    draft.user = action.payload.user;
+    draft.is_login = true;
+  }),
+  [LOG_OUT]: (state, action) => produce(state, (draft) => {
+    deleteCookie('is_login');
+    draft.user = null;
+    draft.is_login = false;
+  }),
+  [GET_USER]: (state, action) => produce(state, (draft) => {}),
+}, initialState);
+
+// action creator export
+const actionCreators = {
+  logIn,
+  logOut,
+  getUser,
+}
+export { actionCreators };
